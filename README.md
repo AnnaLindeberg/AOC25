0def4cce20aee861a3977e995345c9b305bebf60
AnnaLindeberg <anna@familjenlindeberg.com>
2025-12-04 12:04:25 +0100

    day 1

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Advent of Code 2025
-December has started, I forgot, but let's go!
+December has started, I forgot, but let's go! The initial idea was to learn Rust by doing but now I think that will take too much time. Instead, It'll be python, hacky and fast. 
+
 ## Day 1: Secret Entrance
+Day one and mistake one: open the big input file and do not assume your integers lie in the range [0,99].
+
--- day1.py
+++ day1.py
@@ -2,13 +2,31 @@
 # https://adventofcode.com/2024/day/1
 
 def main():
-    res1, res2 = None, None
-    with open("small_input1.txt") as file:
+    res1, res2 = 0, 0
+    dial = 50
+    with open("input1.txt") as file:
         for row in file:
-            pass
+            direction = row[0]
+            steps = int(row[1:].strip())
+            res2 += steps//100
+            steps = steps % 100
+            if direction == 'R':
+                if dial + steps > 100:
+                    res2 += 1
+                dial = (dial + steps) % 100
+            else:
+                if dial > 0 and dial - steps < 0:
+                    res2 += 1
+                dial = (dial - steps) % 100
+            
+            if dial == 0:
+                res2 += 1
+                res1 += 1
+
 
     print(f"Task 1: {res1}\nTask 2: {res2}")
 
+# 2782 too low 7015 too low
 
 if __name__ == '__main__':
     main()
