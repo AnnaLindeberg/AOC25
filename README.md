5c811cab7ba3967b0105b8e5236078579401524e
AnnaLindeberg <anna@familjenlindeberg.com>
2025-12-04 11:34:45 +0100

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Advent of Code 2025
+December has started, I forgot, but let's go!
---
+++ template.py
@@ -0,0 +1,14 @@
+# Day XX of Advent of Code 2024: YY
+# https://adventofcode.com/2024/day/XX
+
+def main():
+    res1, res2 = None, None
+    with open("small_inputXX.txt") as file:
+        for row in file:
+            pass
+
+    print(f"Task 1: {res1}\nTask 2: {res2}")
+
+
+if __name__ == '__main__':
+    main()
